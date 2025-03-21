8a924fc1194d259a48fc89456f6fc3e77d1d2512
udehlee <ritzeepita@gmail.com>
2025-03-21 09:07:55 +0100

    Switch output format from WAV to PCM better compatibility with fingerprinting

--- internals/audio/audio.go
+++ internals/audio/audio.go
@@ -12,24 +12,22 @@ type AudioService struct {
 	Log zerolog.Logger
 }
 
-// ConvertToMonoWAV converts audio bytes to mono WAV format
-func (a AudioService) ConvertToMonoWAV(audioBytes []byte) ([]byte, error) {
+// ConvertToMonoPCM converts audio bytes to raw PCM format
+func (a AudioService) ConvertToMonoPCM(audioBytes []byte) ([]byte, error) {
 	input := bytes.NewReader(audioBytes)
 	output := &bytes.Buffer{}
 
-	a.Log.Info().Msg("converting audio to mono WAV")
-
+	a.Log.Info().Msg("Converting audio to PCM")
 	err := ffmpeg_go.Input("pipe:0").
-		Output("pipe:1", ffmpeg_go.KwArgs{"ac": "1", "ar": "16000", "f": "wav"}).
+		Output("pipe:1", ffmpeg_go.KwArgs{"ac": "1", "ar": "16000", "f": "s16le", "t": "30"}).
 		WithInput(input).
 		WithOutput(output).
 		Run()
-
 	if err != nil {
-		a.Log.Error().Err(err).Msg("Failed to convert audio to mono WAV")
-		return nil, fmt.Errorf("audio conversion failed: %w", err)
+		a.Log.Error().Err(err).Msg("Failed to convert audio to PCM")
+		return nil, fmt.Errorf("audio conversion to PCM failed: %w", err)
 	}
 
-	a.Log.Info().Msg("Successfully converted audio to mono WAV")
+	a.Log.Info().Msg("Successfully converted audio to PCM")
 	return output.Bytes(), nil
 }
